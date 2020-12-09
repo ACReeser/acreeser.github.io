d0e197d84e87359acd8472669bb35ebb1690cb41
Alex Reeser <alexcreeser@gmail.com>
2020-12-08 21:07:27 -0600

    Adding watch script

--- scripts/static-build.js
+++ scripts/static-build.js
@@ -10,13 +10,13 @@ let template = '';
 fse.readFile('./templates/index.dot.html', 'utf-8').then((_template) => {
   // read page template
   template = _template;
-  console.log(template);
   return globP('**/*.partial.html', { cwd: `${srcPath}/partials` })
 }).then((files) => {
   files.forEach((file) => {
     console.log(`found partial ${file}`);
     
     fse.readFile(`${srcPath}/partials/${file}`, 'utf-8').then((content) => {
+      console.log(`writing partial ${file}`);
       fse.writeFile(`${destPath}/${file.replace('.partial', '')}`, template.replace('{{partial}}', content))
     });
   });
