87278c4cc5002feb979cb84533edc2808338a06c
Alex Reeser <alexcreeser@gmail.com>
2020-12-08 20:56:15 -0600

    Remove zetzer, custom build script

--- scripts/static-build.js
+++ scripts/static-build.js
@@ -2,36 +2,23 @@ const fse = require('fs-extra')
 const path = require('path')
 const { promisify } = require('util')
 const globP = promisify(require('glob'))
-const config = require('../site.config')
 
-const srcPath = '.'
-const distPath = '.'
+const srcPath = '.';
+const destPath = '.';
+let template = '';
 
-fse.readFile('./templates/index.dot.html').then((err, data) => {
+fse.readFile('./templates/index.dot.html', 'utf-8').then((_template) => {
+  // read page template
+  template = _template;
+  console.log(template);
+  return globP('**/*.partial.html', { cwd: `${srcPath}/partials` })
+}).then((files) => {
+  files.forEach((file) => {
+    console.log(`found partial ${file}`);
+    
+    fse.readFile(`${srcPath}/partials/${file}`, 'utf-8').then((content) => {
+      fse.writeFile(`${destPath}/${file.replace('.partial', '')}`, template.replace('{{partial}}', content))
+    });
+  });
 })
-
-// read page templates
-globP('**/*.partial.html', { cwd: `${srcPath}/partials` })
-  .then((files) => {
-    files.forEach((file) => {
-      const fileData = path.parse(file)
-      const destPath = path.join(distPath, fileData.dir)
-
-      // create destination directory
-      fse.mkdirs(destPath)
-        .then(() => {
-          // render page
-          return ejsRenderFile(`${srcPath}/pages/${file}`, Object.assign({}, config))
-        })
-        .then((pageContents) => {
-          // render layout with page contents
-          return ejsRenderFile(`${srcPath}/layout.ejs`, Object.assign({}, config, { body: pageContents }))
-        })
-        .then((layoutContent) => {
-          // save the html file
-          fse.writeFile(`${destPath}/${fileData.name}.html`, layoutContent)
-        })
-        .catch((err) => { console.error(err) })
-    })
-  })
-  .catch((err) => { console.error(err) })
+.catch((err) => { console.error(err) })
